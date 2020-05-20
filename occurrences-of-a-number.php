3e78edb9920e570dea8da4dc8e26aee95b8303c5
sergey <tsn3ps@gmail.com>
2020-05-21 01:24:46 +0300

    first commit

---
+++ occurrences-of-a-number.php
@@ -0,0 +1,13 @@
+﻿Checking the number of occurrences of a number in a number
+
+
+<?php
+$number = 12174215471527;
+$int = 1;
+echo "Out of $number:"."<br />".
+    substr_count($number, $int). " times included number - $int";
+?>
+
+
+
+
